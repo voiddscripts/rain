1f670c7ba4b3c9f521ca46097e6b25b6c6c212ae
voidd <94987124+voiddscripts@users.noreply.github.com>
2023-07-11 15:57:17 +0200

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 142823291  then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/voiddscripts/rain/main/mm2.lua"))()
+elseif game.PlaceId == 3101667897 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/voiddscripts/rain/main/los.lua"))()
+end
+end
